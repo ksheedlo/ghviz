4cda1d70d70dcc4a2dbbd5112709e784e3684bcd
Ken Sheedlo <ovrkenthousand@gmail.com>
2016-03-06 15:41:29 -0800

    Add more github tests

--- github/github_test.go
+++ github/github_test.go
@@ -4,14 +4,28 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func pathAndQueryOnly(t *testing.T, rawurl string) string {
+	urlObj, err := url.Parse(rawurl)
+	assert.NoError(t, err)
+	if urlObj.RawQuery != "" {
+		return fmt.Sprintf("%s?%s", urlObj.Path, urlObj.RawQuery)
+	}
+	return urlObj.Path
+}
+
 func TestListStargazers(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		assert.Equal(t, r.Header.Get("Accept"), "application/vnd.github.v3.star+json")
+		assert.Equal(t, "application/vnd.github.v3.star+json", r.Header.Get("Accept"))
+		assert.Equal(t,
+			"/repos/angular/angular/stargazers?per_page=100",
+			pathAndQueryOnly(t, r.URL.String()),
+		)
 		fmt.Fprintln(w, "[{}, {}, {}]")
 	}))
 	defer ts.Close()
@@ -20,4 +34,44 @@ func TestListStargazers(t *testing.T) {
 	allStargazers, err := gh.ListStargazers("angular", "angular")
 	assert.NoError(t, err)
 	assert.Equal(t, len(allStargazers), 3)
+}
+
+func TestPagination(t *testing.T) {
+	var nextPage string
+
+	call := 0
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		call++
+		if call < 2 {
+			w.Header().Add("Link", fmt.Sprintf("<%s>; rel=\"next\"", nextPage))
+		} else {
+			assert.Equal(t, pathAndQueryOnly(t, nextPage), r.URL.String())
+		}
+		fmt.Fprintln(w, "[{}, {}, {}]")
+	}))
+	defer ts.Close()
+	nextPage = fmt.Sprintf("%s/repos/angular/angular/stargazers?per_page=100&page=2", ts.URL)
+
+	gh := NewClientWithBaseUrl("tester", "secretlel", ts.URL)
+	allStargazers, err := gh.ListStargazers("angular", "angular")
+	assert.NoError(t, err)
+	assert.Equal(t, call, 2)
+	assert.Equal(t, len(allStargazers), 6)
+}
+
+func TestListIssues(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "application/vnd.github.v3.star+json", r.Header.Get("Accept"))
+		assert.Equal(t,
+			"/repos/lodash/lodash/stargazers?per_page=100",
+			pathAndQueryOnly(t, r.URL.String()),
+		)
+		fmt.Fprintln(w, "[{}, {}, {}, {}]")
+	}))
+	defer ts.Close()
+
+	gh := NewClientWithBaseUrl("tester", "secretlel", ts.URL)
+	allStargazers, err := gh.ListStargazers("lodash", "lodash")
+	assert.NoError(t, err)
+	assert.Equal(t, len(allStargazers), 4)
 }
