ca2f4281c206bfdbcaaedf1ddfe3d4c762bf0483
Ken Sheedlo <ovrkenthousand@gmail.com>
2016-03-06 14:22:17 -0800

    refactor test case with stretchr/testify/assert

--- github/github_test.go
+++ github/github_test.go
@@ -5,23 +5,19 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestListStargazers(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Accept") != "application/vnd.github.v3.star+json" {
-			t.Errorf("Expected 'Accept: application/vnd.github.v3.star+json', but it was '%s'", r.Header.Get("Accept"))
-		}
+		assert.Equal(t, r.Header.Get("Accept"), "application/vnd.github.v3.star+json")
 		fmt.Fprintln(w, "[{}, {}, {}]")
 	}))
 	defer ts.Close()
 
 	gh := NewClientWithBaseUrl("tester", "secretlel", ts.URL)
 	allStargazers, err := gh.ListStargazers("angular", "angular")
-	if err != nil {
-		t.Error(err)
-	}
-	if len(allStargazers) != 3 {
-		t.Errorf("Expected len(allStargazers) to be 3, but it was %d\n", len(allStargazers))
-	}
+	assert.NoError(t, err)
+	assert.Equal(t, len(allStargazers), 3)
 }
